c5e1b2ef73fa182892a0c46356a022fba12fa145
Oliver Kelly <tkfun2@me.com>
2016-02-02 22:32:45 +0000

    additional work

--- smartapps/toliver182/kodi-manager-callbacks.src/kodi-manager-callbacks.groovy
+++ smartapps/toliver182/kodi-manager-callbacks.src/kodi-manager-callbacks.groovy
@@ -28,12 +28,13 @@ definition(
 preferences {
 	page(name: "pgSettings")
     page(name: "pgURL") 
+    page(name: "pgLights") 
    }
    
    //PAGES
 ///////////////////////////////
 def pgSettings() {
-    dynamicPage(name: "pgSettings", title: "Settings") {
+    dynamicPage(name: "pgSettings", title: "Settings",uninstall: true, install: true) {
       section("Kodi Client"){
       	input "clientName", "text", "title": "Client Name", multiple: false, required: true
   		input "kodiIp", "text", "title": "Kodi IP", multiple: false, required: true
@@ -41,9 +42,16 @@ def pgSettings() {
     	input "kodiUsername", "text", "title": "Kodi Username", multiple: false, required: false
     	input "kodiPassword", "password", "title": "Kodi Password", multiple: false, required: false
     	input "theHub", "hub", title: "On which hub?", multiple: false, required: true
-  }
+        }
+          section("Configure Lights"){
+        	href( "pgLights", description: "Configure lights based on Kodi state", title: "")
+        }
         section("View URLs"){
         	href( "pgURL", description: "Click here to view URLs", title: "")
+        }
+        section("Name")
+        {                    label title: "Assign a name", required: false
+
         }
     }
 }
@@ -60,10 +68,31 @@ def pgURL(){
             input "stopvalue", "text", title:"Web address to copy for stop command:", required: false, defaultValue:"${url}stop"
             input "pausevalue", "text", title:"Web address to copy for pause command:", required: false, defaultValue:"${url}pause"
             input "resumevalue", "text", title:"Web address to copy for resume command:", required: false, defaultValue:"${url}resume"
+            input "allvalue", "text", title:"All Commands", required: false, defaultValue:"Play:${url}play###Stop:${url}stop###Pause:${url}pause###Resume:${url}resume"
+
+
             paragraph "If you have more than one Kodi install, you may install an additional copy of this app for unique addresses specific to each room."
         }
     }
 }
+def pgLights(){
+    dynamicPage(name: "pgLights", title: "Lights", install: false) {
+    section("Enable Light Control?") {
+        	input "shouldControlLights", "bool", title: "Enable light control?", multiple: false, required: true, submitOnChange: true, defaultValue: false
+}
+if(shouldControlLights){
+        section("Lights to Control") {
+            input "switches", "capability.switch", required: true, title: "Which Switches?", multiple: true
+        }
+        section("Level to set Lights to (101 for last known level):") {
+            input "playLevel", "number", required: true, title: "On Playback", defaultValue:"0"
+            input "pauseLevel", "number", required: true, title: "On Pause", defaultValue:"40"
+            input "resumeLevel", "number", required: true, title: "On Resume", defaultValue:"0"
+            input "stopLevel", "number", required: true, title: "On Stop", defaultValue:"101"
+        }
+        }
+        }
+        }
 //END PAGES
 /////////////////////////
 
@@ -80,11 +109,33 @@ def installed() {
 
 def initialize() {
 checkKodi();
-subscribe(location, null, response, [filterEvents:false])  
+
+//if the ip changes we need to remove the old device id.
+getChildDevices().each { childDevice ->
+def deviceNetID = childDevice.deviceNetworkId
+log.debug "my id: "+ NetworkDeviceId()
+log.debug "net id: " + deviceNetID
+if(deviceNetID != NetworkDeviceId()){
+log.debug "removing: " + deviceNetID
+deleteChildDevice(deviceNetID)
+}
+}
+
 }
 
 def updated() {
 unsubscribe();
+/*
+getChildDevices().each { childDevice ->
+def deviceNetID = childDevice.deviceNetworkId
+log.debug "my id: "+ NetworkDeviceId()
+log.debug "net id: " + deviceNetID
+if(deviceNetID != NetworkDeviceId()){
+log.debug "removing: " + deviceNetID
+deleteChildDevice(deviceNetID)
+}
+}*/
+
 initialize()
 
 }
@@ -108,13 +159,17 @@ mappings {
 			GET: "stateIsPause"
 		]
 	}
-    path("/stateIsPause") {
+    path("/resume") {
         action: [
-            GET: "resume"
+            GET: "stateIsPause"
         ]
 	}  
 }
 void stateIsPlay() {
+if("$settings.shouldControlLights" == "true"){
+    RunCommand(playLevel)
+}
+
 	//Code to execute when playback started in KODI
     log.debug "Play command started"
 	//Find client
@@ -125,6 +180,9 @@ void stateIsPlay() {
     getPlayingtitle()
 }
 void stateIsStop() {
+if("$settings.shouldControlLights" == "true"){
+    RunCommand(stopLevel)
+}
 	//Code to execute when playback stopped in KODI
     log.debug "Stop command started"
    	//Find client
@@ -134,6 +192,9 @@ void stateIsStop() {
     KodiClient.setPlaybackState("stopped")
  }
 void stateIsPause() {
+if("$settings.shouldControlLights" == "true"){
+    RunCommand(pauseLevel)
+}
 	//Code to execute when playback paused in KODI
     log.debug "Pause command started"
    	//Find client
@@ -144,6 +205,9 @@ void stateIsPause() {
     getPlayingtitle()
 }
 void stateIsResume() {
+if("$settings.shouldControlLights" == "true"){
+    RunCommand(resumeLevel)
+}
 	//Code to execute when playback resumed in KODI
     log.debug "Resume command started"
 	//Find client
@@ -338,4 +402,93 @@ def String getKodiAddress(deviceNetworkId) {
 def ip = deviceNetworkId.replace("KodiClient:", "");
 	def parts = ip.tokenize('.');
 	return parts[0] + "." + parts[1] + "." + parts[2] + "." + parts[3];
+}
+
+//Lighting control
+
+private void RunCommand(level){
+	//Check to see if current mode is in white/black list before we do anything
+	if((!onlyModes || location.currentMode in onlyModes) && !(location.currentMode in neverModes)){
+    	//Mode is good, go ahead with commands
+    	if (level == 101){
+            log.debug "Restoring Last Known Light Levels"
+            restoreLast(switches)
+        }else if (level <= 100){
+            log.debug "Setting lights to ${level} %"
+            SetLight(switches,level)
+    	}
+    }
+}
+
+private void restoreLast(switchList){
+	//This will look for the last external (not from this app) setting applied to each switch and set the switch back to that
+	//Look at each switch passed
+	switchList.each{sw ->
+    	def lastState = LastState(sw) //get Last State
+        if (lastState){   //As long as we found one, set it    
+            SetLight(sw,lastState)
+    	}else{ //Otherwise assume it was off
+        	SetLight(sw, "off") 
+        }
+    }
+}
+
+private def LastState(device){
+	//Get events for this device in the last day
+	def devEvents = device.eventsSince(new Date() - 1, [max: 1000])
+    //Find Last Event Where switch was turned on/off/level, but not changed by this app
+    //Oddly not all events properly contain the "installedSmartAppId", particularly ones that actual contain useful values
+    //In order to filter out events created by this app we have to find the set of events for the app control and the actual action
+    //the first 8 char of the event ID seem to be unique much of the time, but the rest seems to be the same for any grouping of events, so match on that (substring)
+    //In case the substring fails we will also check for events with similar timestamp (within 8 sec)
+    def last = devEvents.find {
+        (it.name == "level" || it.name == "switch") && (devEvents.find{it2 -> it2.installedSmartAppId == app.id && (it2.id.toString().substring(8) == it.id.toString().substring(8) || Math.sqrt((it2.date.getTime() - it.date.getTime())**2) < 6000 )} == null)
+        }
+    //If we found one return the stringValue
+    if (last){
+    	log.debug "Last External Event - Date: ${last.date} | Event ID: ${last.id} | AppID: ${last.installedSmartAppId} | Description: ${last.descriptionText} | Name: ${last.displayName} (${last.name}) | App: ${last.installedSmartApp} | Value: ${last.stringValue} | Source: ${last.source} | Desc: ${last.description}"
+    	//if event is "on" find last externally set level as it could be in an older event
+        if(last.stringValue == "on"){
+        	devEvents = device.eventsSince(new Date() - 7, [max: 1000]) //Last level set command could have been awhile back, look in last 7 days
+            def lastLevel = devEvents.find {
+            (it.name == "level") && (devEvents.find{it2 -> it2.installedSmartAppId == app.id && (it2.id.toString().substring(8) == it.id.toString().substring(8) || Math.sqrt((it2.date.getTime() - it.date.getTime())**2) < 6000 )} == null)
+            }
+        	if(lastLevel){
+            	return lastLevel.stringValue 
+            }
+        }
+		return last.stringValue
+    }else{
+    	return null
+    }
+}
+
+private void SetLight(switches,value){
+	//Set value for one or more lights, translates dimmer values to on/off for basic switches
+
+	//Fix any odd values that could be passed
+	if(value.toString().isInteger()){
+    	if(value.toInteger() < 0){value = 0}
+        if(value.toInteger() > 100){value = 100}
+    }else if(value.toString() != "on" && value.toString() != "off"){
+    	return //ABORT! Lights do not support commands like "Hamster"
+    }
+	switches.each{sw ->
+    	log.debug "${sw.name} |  ${value}"
+    	if(value.toString() == "off" || value.toString() == "0"){ //0 and off are the same here, turn the light off
+        	sw.off()
+        }else if(value.toString() == "on"  || value.toString() == "100"){ //As stored light level is not really predictable, on should mean 100% for now
+        	if(sw.hasCommand("setLevel")){ //setlevel for dimmers, on for basic
+            	sw.setLevel(100)
+            }else{
+            	sw.on()
+            }
+        }else{ //Otherwise we should have a % value here after cleanup above, use ir or just turn a basic switch on
+        	if(sw.hasCommand("setLevel")){//setlevel for dimmers, on for basic
+            	sw.setLevel(value.toInteger())
+            }else{
+            	sw.on()
+            }
+        }
+    }
 }
